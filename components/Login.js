0f1013119c8d6748bb20e7c8ba2afacd08b007ff
Shibin Raju Mathew <shibinrajumathew@gmail.com>
2018-07-02 13:10:34 +0530

    Login page updated with few new features

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,6 @@
-/* @flow */
-
+/* Login page for practz no sign up 
+*  Author: shibin
+*/
 import React, { Component } from 'react';
 import {
   View,
@@ -17,39 +18,20 @@ import {
   AsyncStorage,
 } from 'react-native';
 
-
-
-
-
-
 export default class Login extends Component {
   constructor(){
-    //inherit parent props
     super();
-    //create dynamic variables
     this.state={
-      movie:'',
-      xtest:'',
-      f_status:'',
-      text:' ',
+      emailid:'',
       pass:'',
-      rol:'role',
-      btn: true,
-      logins:'not here',
-      search_id:'',
     }
   }
 
   static navigationOptions = {
     title:'Please Login',
-  //   title: (<Image source={require('./images/pro_logo_ins.png')}
-  //
-  // /> ),
-  headerStyle:{
+    headerStyle:{
     display:'none',},
-  headerTitleStyle: {
-    // marginLeft:'auto',
-    // marginRight:'auto',
+    headerTitleStyle: {
     fontWeight:'bold',
     display:'none',
   },
@@ -58,84 +40,42 @@ export default class Login extends Component {
 
 
 checkUser(){
-
-
-  fetch('http://medico.vveeo.com/index.php?userid='+this.state.text+'&pass='+this.state.pass)
-  //get status and append with data and return as one object
+  console.log("onCheck user:");
+  fetch('https://ideal.dev.practz.com/practz/login', {
+  method: 'post',
+  headers: {
+    'Accept': 'application/json, text/plain,',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    "username" :"orgadmin1@mozanta.com",
+    "password":"practz",
+    "appId":"ILEARN",
+    "domainName":"ideal.dev.practz.com"
+  })
+})
   .then(response =>  response.json())
   .then(responseobj => {
     this.setState({
-      search_id:responseobj.search_id,
-      uid:responseobj.uid,
-      uname: responseobj.username,
-      xtest: responseobj[responseobj.username][this.state.rol],
+
     });
-
-    if(responseobj[responseobj.username][this.state.rol]==4){
-      AsyncStorage.multiSet([
-      ["hospital",responseobj[responseobj.username][this.state.attr]],
-      ["search",this.state.search_id],
-      ["uid",this.state.uid],
-
-      ]);
-
-    }else{
-      AsyncStorage.multiSet([
-      ["search",this.state.search_id],
-      ["uid",this.state.uid],
-      ]);
-    }
+    console.log("return----login status:",responseobj.success);
 
 
-    console.log("return----xtest---",this.state.xtest);
-
-
-    this.props.navigation.navigate(this.reDirect());
+    // this.props.navigation.navigate(this.reDirect());
   })
-
-}
-reDirect(){
-
-
-  if(this.state.xtest==1)
-  {  return 'Admin';
-  this.setState({xtest:''});
-}
-else if(this.state.xtest==2){
-  return 'Doc';
-  this.setState({xtest:''});
-}
-else if(this.state.xtest==3){
-  return 'Patient';
-  this.setState({xtest:''});
-}else if(this.state.xtest==4){
-
-
-  return 'HospitalAdmin';
-  this.setState({xtest:''});
-}
-else if(this.state.xtest==5){
-  return 'Pharmacist';
-  this.setState({xtest:''});
-}
-else if(this.state.xtest==6){
-  return 'MedicalRep';
-  this.setState({xtest:''});
-}
-else if(this.state.xtest==7){
-  return 'LabAsst';
-  this.setState({xtest:''});
-}
-else {
-  Alert.alert("Wrong user");
-  return 'Home';
-  this.setState({xtest:''});
-}
+  .catch((error) => {
+      console.error(error);
+    });
 }
 
-
+//redirect function for future need
+//reDirect(){
+//  //body to reDirect
+//}
 
 componentDidMount() {
+  //internet connection check
   NetInfo.isConnected.fetch().then(isConnected => {
   console.log('First, is ' + (isConnected ? 'online' : 'offline'));
   if ( isConnected )
@@ -147,64 +87,67 @@ componentDidMount() {
     Alert.alert("Net info alert","Please connnect to internet for further app use");
 
   }
-
-
-
 });
 
 
-//Get session
-AsyncStorage.multiGet(['user', 'role_no']).then((data) => {
-    let user = data[0][1];
+//Get session for future use do not rewrite
+// AsyncStorage.multiGet(['user', 'role_no']).then((data) => {
+//     let user = data[0][1];
+//
+//     let role_no = data[1][1];
+// //navigate('Admin')
+//     if (role_no !== null)
+//     {
+//       this.setState({xtest:data[1][1]});
+//       this.props.navigation.navigate(this.reDirect());
+//
+//
+//     }
+//
+// });
 
-    let role_no = data[1][1];
-//navigate('Admin')
-    if (role_no !== null)
-    {
-      this.setState({xtest:data[1][1]});
-      this.props.navigation.navigate(this.reDirect());
-
-
-    }
-
-});
-
-
-/*
-// Delete session
-logout(){
-let keys = ['user', 'role_no'];
-AsyncStorage.multiRemove(keys, (err) => {
-    ('Local storage user info removed!');
-});
 }
 
-*/
-}
-
-
-componentWillUnmount() {
-  /* */
-}
   render() {
 
 
       const { navigate } = this.props.navigation;
     return (
-      <KeyboardAvoidingView behavior="padding"  style={styles.container}>
+      <ScrollView behavior="padding"  style={styles.container}>
         <View style={styles.logoContainer}>
         <Image source={require('./img/logo2.png')}
         style={styles.img}
       />
+      <Text style={{
+            color: 'white',
+            fontSize:12,
+          }}>digitalising education
+        </Text>
+      </View>
+      <View style={styles.tabForm}>
+        <View style={styles.selectedInnerTabForm}>
+          <Text style={{
+              color: '#8bc34a',
+              fontWeight: '500',
+              fontSize:12,
+            }}>Sign in
+          </Text>
+        </View>
+        <View style={styles.innerTabForm}>
+          <Text style={{
+              color: 'white',
+              fontWeight: '500',
+              fontSize:12,
+            }}>Sign Up
+          </Text>
+        </View>
       </View>
       <View style={styles.myForm}>
-
-
         <TextInput
           style={styles.input}
           underlineColorAndroid="transparent"
-          onChangeText={(text) => this.setState({text})}
-          placeholder="Email"
+          onChangeText={(text) => this.setState({emailid:text})}
+          placeholder="Email ID"
         />
         <TextInput
           underlineColorAndroid="transparent"
@@ -214,18 +157,22 @@ componentWillUnmount() {
         />
 
         <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Dash')}
+          onPress={() => this.checkUser()}
           title="Home" style={styles.buttonContainer}
           >
-          <Text style={styles.buttonText}  >Login</Text>
+          <Text style={styles.buttonText}  >Let me in</Text>
         </TouchableOpacity>
-
+        <View style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+      }}>
+          <Text style={{
+            color: 'white',
+          }}  >Forgot password ?</Text>
           </View>
-
-
-
-
-  </KeyboardAvoidingView>
+        </View>
+  </ScrollView>
 
     );
   }
@@ -246,13 +193,40 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop:50,
+    marginTop:20,
+  },
+  tabForm: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:20,
+    marginLeft: 8,
+    flexDirection: 'row',
+  },
+  innerTabForm: {
+    width: Dimensions.get('window').width/2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:20,
+    marginLeft: 5,
+    paddingBottom:15,
+    flexDirection: 'row',
+  },
+  selectedInnerTabForm: {
+    width: Dimensions.get('window').width/3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:20,
+    marginLeft: 5,
+    paddingBottom:15,
+    flexDirection: 'row',
+    borderColor:'#8bc34a',
+    borderBottomWidth: 5,
   },
   myForm: {
-    flex:1.5,
-    marginTop:20,
-    padding: 20,
-  },
+      flex:1.5,
+      marginTop:20,
+      padding: 20,
+    },
   formTitle:{
     flex:2,
     fontSize: 22,
@@ -262,7 +236,7 @@ const styles = StyleSheet.create({
   input:{
     height: 60,
     padding: 20,
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#fff',
     marginBottom: 10,
   },
   buttonContainer:{
