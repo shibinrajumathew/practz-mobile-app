25b06deb8a8d3a868d807c99055ebcec27a55367
Shibin Raju Mathew <shibinrajumathew@gmail.com>
2018-07-31 17:04:14 +0530

    Revert "some file missing issue"
    This reverts commit be2e9bc3165f5501a81c8f86d445493c2186eb96.

--- components/Assets/Style.js
+++ components/Assets/Style.js
@@ -419,12 +419,6 @@ export default StyleSheet.create({
    marginLeft:15,
    marginTop:10,
    marginRight:5
- },
- title:{
-   fontSize:20,
-   fontFamily: 'Avenir, Heavy',
-    fontWeight: 'bold',
-    color: '#000'
  }
 
 });
