c17d9ec94fb5531f3dfa87dc04dd2bacd58042e2
harikrishnansp <harikrishnansp123@gmail.com>
2018-08-09 10:54:42 +0530

    reviewed alignment

--- components/AvailableExams.js
+++ components/AvailableExams.js
@@ -11,21 +11,21 @@ import URL from './Url';
 import styles from './Assets/Style';
 export default class AvailableExams extends Component<Props> {
   constructor() {
-       super()
-       this.state = {
-        HOME:URL.HOME,
-        AVAILABLE_EXAMS:URL.AVAILABLE_EXAMS,
-        availableExamList:[
-          {
-            "questionPaperName": "",
-            "totalExamMarks":"",
-            "expiryDate": "",
-            "duration":"",
-            "attributes": {
-              "questionPaperName": "",
-            },
-          }
-        ]
+    super()
+    this.state = {
+      HOME:URL.HOME,
+      AVAILABLE_EXAMS:URL.AVAILABLE_EXAMS,
+      availableExamList:[
+      {
+        "questionPaperName": "",
+        "totalExamMarks":"",
+        "expiryDate": "",
+        "duration":"",
+        "attributes": {
+          "questionPaperName": "",
+        },
+      }
+      ]
     }
   }
   componentWillMount() {
@@ -50,32 +50,32 @@ export default class AvailableExams extends Component<Props> {
       color:'#5e3f8c',
       textAlign: 'center',
     },
-};
+  };
   render() {
-     var examList = this.state.availableExamList.map(function(exam){
+    var examList = this.state.availableExamList.map(function(exam){
       return (<View key={(exam, index) => index.toString()} >
-       <View style={[styles.announcementBox]}>
+        <View style={[styles.announcementBox]}>
           <View style={[styles.flexrow]}>
-              <View style={{flex: 2}} >
-                 <Text style={[styles.title,styles.margins]}>{exam.attributes.questionPaperName}</Text>
-              </View>
-              <View style={[styles.sideBotton, styles.brightBlue]} >
-                 <Text style={[styles.bookFont,styles.whiteFont]} >Science & Tech </Text>
-              </View>
+            <View style={{flex: 2}} >
+              <Text style={[styles.title,styles.margins]}>{exam.attributes.questionPaperName}</Text>
+            </View>
+            <View style={[styles.sideBotton, styles.brightBlue]} >
+              <Text style={[styles.bookFont,styles.whiteFont]} >Science & Tech </Text>
+            </View>
           </View>
-              <Text style={[stylish.myview]}>Staff Board Exam </Text>
-              <Text style={[stylish.myview]}>No of Question <Text style={[styles.count]}> {exam.totalExamMarks}  </Text><Text style={[stylish.myview]}>        Time Allocated<Text style={[styles.count]}>  {exam.duration} </Text></Text></Text>
-              <Text style={[stylish.container]}>End on {exam.expiryDate}</Text>
+          <Text style={[stylish.myview]}>Staff Board Exam </Text>
+          <Text style={[stylish.myview]}>No of Question <Text style={[styles.count]}> {exam.totalExamMarks}  </Text><Text style={[stylish.myview]}>        Time Allocated<Text style={[styles.count]}>  {exam.duration} </Text></Text></Text>
+          <Text style={[stylish.container]}>End on {exam.expiryDate}</Text>
         </View>
       </View>);
-           })
+    })
     return (
       <ScrollView style={{backgroundColor:'#FFFFFF',}}>
-           <Text style={{fontSize:15,color:'#000000',marginLeft:50,marginTop:15,marginBottom:10}}>Available Exams</Text>
-           {examList}
+        <Text style={{fontSize:15,color:'#000000',marginLeft:50,marginTop:15,marginBottom:10}}>Available Exams</Text>
+        {examList}
       </ScrollView>
     );
-    }
+  }
 }
 const stylish = StyleSheet.create({
   myview: {
@@ -91,4 +91,4 @@ const stylish = StyleSheet.create({
     marginTop:7,
     marginBottom:10
   }
-    });
+});
